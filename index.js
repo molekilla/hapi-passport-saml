61670a735a90cff871430375e96e2d48f9733f97
Rogelio Morrell <molekilla@gmail.com>
2018-07-11 17:28:17 -0500

    refactoring code. SAML flow with assert replying with profile

--- index.js
+++ index.js
@@ -1,53 +1,27 @@
-const scheme = require('./lib/scheme');
-const samlCtrl = require('./lib');
-const saml = require('passport-saml/lib/passport-saml/saml');
-class HapiSaml {
-    constructor() {
-        this.saml = null;
-        this.props = {};
-    }
-
-    load(options) {
-        if (!options.saml) {
-            throw new Error('Missing options.saml');
-        }
-        if (!options.metadata) {
-            throw new Error('Missing options.metadata');
-        }
-        if (!options.onAssertRequest) {
-            throw new Error('Missing options.onAssertRequest');
-        }
-        if (!options.onAssertResponse) {
-            throw new Error('Missing options.onAssertResponse');
-        }
-        this.saml = new saml.SAML(options.saml);
-        this.props = { ...options.saml };
-        this.props.decryptionCert = options.decryptionCert;
-    }
-
-    getSamlLib() {
-        return this.saml;
-    }
-}
+const SchemeImpl = require('./lib/SchemeImpl');
+const SamlController = require('./lib/SamlController');
+const HapiSaml = require('./lib/HapiSaml');
 
 const register = function (server, options, next) {
-    const hapiSaml = new HapiSaml();
-    hapiSaml.load(options);
+    const hapiSaml = new HapiSaml(options);
+    // hapiSaml.load(options);
 
-    server.auth.scheme('saml', scheme(hapiSaml));
+    server.auth.scheme('saml', SchemeImpl(hapiSaml));
 
+    // SAML metadata
     server.route({
         method: 'GET',
         path: options.metadata.path,
-        handler: samlCtrl.getMetadata(hapiSaml),
+        handler: SamlController.getMetadata(hapiSaml.getSamlLib()),
         config: options.metadata.config,
     });
 
+    // SAML assert
     server.route({
         method: 'POST',
-        path: options.routes.assert.path,
-        handler: samlCtrl.assert(hapiSaml, options.onAssertResponse, options.onAssertRequest),
-        config: options.routes.assert.config,
+        path: options.assert.path,
+        handler: SamlController.assert(hapiSaml.getSamlLib(), options.onAssertResponse, options.onAssertRequest),
+        config: options.assert.config,
     });
 
     next();
