155d5e243374979173b7f19a488648796cd8a11a
Rogelio Morrell <molekilla@gmail.com>
2018-07-11 19:52:07 -0500

    SAML auth flow working

--- index.js
+++ index.js
@@ -4,24 +4,35 @@ const HapiSaml = require('./lib/HapiSaml');
 
 const register = function (server, options, next) {
     const hapiSaml = new HapiSaml(options);
-    // hapiSaml.load(options);
 
-    server.auth.scheme('saml', SchemeImpl(hapiSaml));
+    server.auth.scheme('saml', SchemeImpl(hapiSaml, options));
+    const hapiSamlOptions = options.hapiSaml;
 
     // SAML metadata
     server.route({
         method: 'GET',
-        path: options.metadata.path,
+        path: hapiSamlOptions.routes.initiateAuthentication.path,
+        handler: SamlController.login(hapiSaml.getSamlLib(), options.hapiSaml.cookieName),
+        config: hapiSamlOptions.routes.initiateAuthentication.config,
+    });
+
+    // SAML metadata
+    server.route({
+        method: 'GET',
+        path: hapiSamlOptions.routes.metadata.path,
         handler: SamlController.getMetadata(hapiSaml.getSamlLib()),
-        config: options.metadata.config,
+        config: hapiSamlOptions.routes.metadata.config,
     });
 
     // SAML assert
     server.route({
         method: 'POST',
-        path: options.assert.path,
-        handler: SamlController.assert(hapiSaml.getSamlLib(), options.onAssertResponse, options.onAssertRequest),
-        config: options.assert.config,
+        path: hapiSamlOptions.routes.assert.path,
+        handler: SamlController
+            .assert(hapiSaml.getSamlLib(),
+                hapiSamlOptions.assertHooks.onResponse,
+                hapiSamlOptions.assertHooks.onRequest, , options.hapiSaml.cookieName),
+        config: hapiSamlOptions.routes.assert.config,
     });
 
     next();
